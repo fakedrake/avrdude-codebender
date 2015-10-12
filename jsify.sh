a59d9f108281c935bb80157e0d4f526d61481dae
Chris Perivolaropoulos <cperivol@csail.mit.edu>
2015-10-12 16:31:12 +0300

    Proper jsify script

--- jsify.sh
+++ jsify.sh
@@ -1,1 +1,9 @@
-make -j4 && ./avrdude -p \? -C avrdude.conf -v |& awk 'BEGIN {p = 0} /BEGIN PARTS/{p=1} (p>0) {print}' > parts.js && node parts.js && cat parts.js | python compress.py > parts.min.js && node parts.min.js
+#!/bin/bash
+set -e
+
+make -j4
+./avrdude -C avrdude.conf -J > parts.js
+node parts.js
+cat parts.js | python compress.py > parts.min.js
+node parts.min.js
+wc parts.min.js
